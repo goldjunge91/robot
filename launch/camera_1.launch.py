9f82bf2729596d3c562cf80993b0a20d4ed08dd7
goldjunge91 <30201929+goldjunge91@users.noreply.github.com>
2025-08-30 18:06:32 +0200

    feat: add launch file for v4l2_camera node configuration

---
+++ launch/camera_1.launch.py
@@ -0,0 +1,28 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    return LaunchDescription(
+        [
+            Node(
+                package="v4l2_camera",
+                executable="v4l2_camera_node",
+                output="screen",
+                parameters=[
+                    {
+                        "image_size": [640, 480],
+                        "camera_frame_id": "camera_link_optical",
+                        # Correct parameter name is "video_device"
+                        "video_device": "/dev/v4l/by-id/usb-HD_USB_Camera_HD_USB_Camera-video-index0",
+                        "camera_name": "camera",
+                        "pixel_format": "YUYV",
+                        "camera_info_url": "file:///home/pi/.ros/camera_info/camera.yaml",
+                    }
+                ],
+            )
+        ]
+    )
